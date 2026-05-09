e536481c2914065304685edb4d72bc11f9877f62
Ace <victorajibua14@gmail.com>
2026-05-09 17:56:12 +0100

    modified all files on the hashnode test folder

--- tests/hashnode/article.spec.ts
+++ tests/hashnode/article.spec.ts
@@ -20,19 +20,19 @@ test.use({
 });
 
 test("Hashnode Article Search Test", async ({ page }) => { 
-  test.setTimeout(300_000); // increase timeout for AI execution
+  test.setTimeout(400_000); // increase timeout for AI execution
   await runSteps({
     page,
     userFlow: "Hashnode Article Search Test",
     steps: [
       { description: "Navigate to //https://https://hashnode.com/" },
-      { description: "click on any articles on the feed" },
+      { description: "click on just one articles on the feed" },
     ],
 
     assertions: [
-      { assertion: "an article cover image or thumbnail is visible at the top" },
+      { assertion: "the author profile picture is visible on the page" },
       { assertion: "the author name is visible on the page" },
-      { assertion: "the contents of the article is visible below the cover image or thumbnail"}
+      { assertion: "the content of the article is visible on the page" },
     ],  
     test,
     expect
